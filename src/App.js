8b7dfae6cc896d7abfb690a11241590203f6e69c
nazarchykur <nazarchykur@gmail.com>
2023-09-13 09:23:49 +0300

    added an example of thinking about State and Lifting State Up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Socks2", quantity: 12, packed: true },
+  { id: 3, description: "Shirts", quantity: 5, packed: true },
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    // setItems((items) => items.push(item)); // do not do this, because we are mutating the state
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +28,7 @@ function Logo() {
   return <h1>🌴 FAR FROM 📷</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -34,6 +41,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -63,11 +72,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
@@ -79,8 +88,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.description}
-        {item.quantity}
+        {item.description} {item.quantity}
       </span>
       <button>X</button>
     </li>
