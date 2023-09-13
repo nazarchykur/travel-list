6f6efd30d7a7d2eb18cf3db2edb64566d26352a4
nazarchykur <nazarchykur@gmail.com>
2023-09-13 11:45:52 +0300

    added an example of clearing the list of items

--- src/App.js
+++ src/App.js
@@ -26,6 +26,16 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) {
+      setItems([]);
+    }
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -34,6 +44,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onCrearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -89,7 +100,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onCrearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -129,6 +140,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onCrearList}>Clear Packing List</button>
       </div>
     </div>
   );
